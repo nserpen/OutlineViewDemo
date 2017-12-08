9b6d5290b5132dbe49e62d4a41827bfc09c4c889
Nejat Serpen <anserpen@gmail.com>
2017-12-08 12:51:42 +0300

    Initial Commit

---
+++ OutlineViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutlineViewDemo
+//
+//  Created by Nejat Serpen on 08/12/2017.
+//  Copyright © 2017 Adeo IT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
